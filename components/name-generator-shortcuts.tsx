cde65f62dfeab0fb81ad42fa3d406fe2aa25d56b
folajindayo <temidayofolajin@yahoo.com>
2025-11-04 02:55:54 +0100

    Fix missing Badge import in name-generator-shortcuts

--- components/name-generator-shortcuts.tsx
+++ components/name-generator-shortcuts.tsx
@@ -2,6 +2,7 @@
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
+import { Badge } from '@/components/ui/badge'
 import { Keyboard, Zap, Sparkles } from 'lucide-react'
 
 interface Shortcut {
